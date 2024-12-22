11dde929abad4a7e44ed507e6190dfcaaf0c135b
Arsen Yeremin <arsen3d@gmail.com>
2024-12-22 11:44:42 -0800

    Update

--- examples/gradio/app.py
+++ examples/gradio/app.py
@@ -3,8 +3,37 @@ import os
 import requests
 import tempfile
 import tarfile
+from web3 import Web3
+
 WEB3_PRIVATE_KEY = os.getenv("WEB3_PRIVATE_KEY", "")
 
+# Connect to the RPC endpoint
+web3 = Web3(Web3.HTTPProvider('https://demonet-chain-http.lilypad.tech'))
+account = web3.eth.account.from_key(WEB3_PRIVATE_KEY)
+wallet_address = account.address
+# ERC20 contract address and ABI
+contract_address = '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853'
+contract_abi = [
+    {
+        "constant": True,
+        "inputs": [{"name": "_owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "balance", "type": "uint256"}],
+        "type": "function",
+    }
+]
+
+# Create contract instance
+contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+
+
+def get_balance(wallet_address):
+    try:
+        balance_wei = contract.functions.balanceOf(wallet_address).call()
+        balance_eth = Web3.from_wei(balance_wei, 'ether')
+        return f"{balance_eth:.5f} LP"
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 head = f"""
     <script src="/gradio_api/file=app/app.js"></script>
@@ -66,13 +95,16 @@ gr.set_static_paths(paths=["app"])
 
 with gr.Blocks(head=head) as demo:
     progress = gr.Progress()
-    # with gr.Blocks():
-    #     with gr.Row():
-    #         with gr.Column():
-    #             gr.Label("Select a module to run")
-    #         with gr.Column():    
-    #             gr.Label("Select a module to run")
-    # gr.Image(value="app/gradio.svg", type="pil"),
+    with gr.Blocks():
+        with gr.Row():
+            with gr.Column(scale=1):
+                gr.Label( label="LP Balance",  value=get_balance(wallet_address))
+            with gr.Column(scale=1):    
+                balance_wei = web3.eth.get_balance(wallet_address)
+                balance_eth = Web3.from_wei(balance_wei, 'ether')
+                gr.Label(label="ETH Balance", value=f"{balance_eth:.5f} ETH")
+            with gr.Column(scale=5):    
+                gr.Label(label="Wallet Address", value=wallet_address)
     dropdown = gr.Dropdown([
         ("Cowsay","cowsay:v0.0.4,Message"), 
          ("SDXL","github.com/noryev/module-sdxl-ipfs:ae17e969cadab1c53d7cabab1927bb403f02fd2a,prompt"), 
@@ -81,7 +113,7 @@ with gr.Blocks(head=head) as demo:
         # value="Message",
         interactive=True
     )
-    inp = gr.Textbox(label="Input",placeholder="Prompt!!!")
+    inp = gr.Textbox(label="Input", placeholder="Enter wallet address")
     run_button = gr.Button("Run")
     image = gr.Image( visible=False)
     file = gr.File( visible=False)
@@ -95,6 +127,11 @@ with gr.Blocks(head=head) as demo:
         elem_classes=["monospace"],
         show_copy_button=True)
     err = gr.Textbox(label="Error", visible=False,)
+
+    def on_run_click(wallet_address):
+        balance = get_balance(wallet_address)
+        return balance
+
     run_button.click(
         show_api=False,
         fn=lambda: (gr.Button(interactive=False), gr.Textbox(visible=False), gr.Textbox(visible=False), gr.File(visible=False),gr.Image(visible=False)),  # Disable button first
