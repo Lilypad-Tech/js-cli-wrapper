2ca1d55b07d20dac8cba34d072405e0df45280e0
Arsen Yeremin <arsen3d@gmail.com>
2025-02-19 14:09:01 -0800

    Remove commented-out CORS middleware from index.js

--- src/index.js
+++ src/index.js
@@ -60,12 +60,12 @@ const app = express()
 app.use(cors());
 // middleware
 app.use(express.json())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 // routes
 app.get("/ping", (req, res) => {
   res.send("pong")
